49c2ec7942e6c58387f5493ae115f79e1beba97d
Nitin Lavaniya <82403836+NITIN200208@users.noreply.github.com>
2024-08-21 16:28:02 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        Testing t=new Testing();
+        Test t=new Test();
         String message=t.testing();
         System.out.println(message);
     }
